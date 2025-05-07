584a05dda2fbb0c98476dbeba7ee0bcc2e2851a5
Sufiyan0007 <sufiyanlaxmeshwar71@gmail.com>
2025-05-07 14:20:56 +0530

    save

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,9 @@
 public class Employee {
     String name;
+    String lastname;
     int id;
     double salary;
-    public Employee(String name,int id,double salary)
+    public Employee(String name, String lastname,int id,double salary)
     {
         this.name=name;
         this.id=id;
@@ -11,10 +12,11 @@ public class Employee {
     public void displayInfo(){
         System.out.println("Employee Id:"+ id);
         System.out.println("Employee Name:" + name);
+        System.out.println("Employee LastName:" + lastname);
         System.out.println("Salary:" + salary);
     }
     public static void main(String[] args) {
-        Employee emp1 = new Employee("XYZ",101 , 50000.0);
+        Employee emp1 = new Employee("XYZ","abcd",101 , 50000.0);
         emp1.displayInfo();
     }
 }
